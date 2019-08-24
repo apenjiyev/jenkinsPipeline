fad61b22ea8efc3f6c329661e2fc7a2b23e2a9b5
apenjiyev <azatpenji@gmail.com>
2019-08-24 09:09:48 -0700

    added name

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("hello world"){
-        properties([pipelineTriggers([cron('* * * * *')])])
+        properties([parameters([string(defaultValue: 'Azat', description: 'Paste your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * * ')])])
         sh "echo hello"        
     }
 }

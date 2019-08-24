2931c14c75fd53146b1dcbe2cf5092063a71b1e1
apenjiyev <azatpenji@gmail.com>
2019-08-24 09:11:40 -0700

    added git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("hello world"){
         properties([parameters([string(defaultValue: 'Azat', description: 'Paste your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * * ')])])
-        sh "echo hello"        
+        git 'https://github.com/apenjiyev/jenkinsPipeline.git'        
     }
 }

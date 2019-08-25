0083fc8d446603730a39165ccb20f39195989dd0
Azat Penjiyev <azatpenji@gmail.com>
2019-08-25 13:17:32 -0700

    terraform complete
    terraform complete

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,69 @@
-node {
-    stage("Download Terraform"){
+pipeline{
+
+    agent any
+
+    stages{
+
+        stage("Run Command"){
+
             steps{
-                ws("tmp/"){
-                    script {
-                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
-                        if (exists) {
-                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                        } else {
-                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                        }
-                    }
-                }
+
+                sh '''
+
+                set +xe
+
+                echo Hello
+
+                ech  Error
+
+                sudo yum install httpd wget unzip -y
+
+                ping -c 4 google.com
+
+                '''
+
             }
+
         }
+
+        stage("Download Terraform"){
+
+            steps{
+
+                ws("tmp/"){
+
+                    script {
+
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+
+                        if (exists) {
+
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+
+                            sh "sudo mv terraform /bin"
+
+                            sh "terraform version"
+
+                        } else {
+
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+
+                            sh "sudo mv terraform /bin"
+
+                            sh "terraform version"
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
 }
